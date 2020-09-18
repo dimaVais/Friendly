ebd8de673d5085970804ce3808fa13f068e7ec88
Asaf Dulman <asafdulman@gmail.com>
2020-09-18 10:26:24 +0300

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,66 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+// import { connect } from 'react-redux';
 
-export  class Login extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Login</h1>
-            </div>
-        )
-    }
-}
+// import {
+//     loadUsers,
+//     removeUser,
+//     login,
+//     logout,
+//     signup
+// } from '../actions/userActions';
+
+// class _Login extends Component {
+
+//     state = {
+//         msg: '',
+//         loginCred: {
+//             username: '',
+//             password: ''
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <form onSubmit={this.doLogin}>
+//                     <input
+//                         type="text"
+//                         name="username"
+//                         value={this.state.loginCred.username}
+//                         onChange={this.loginHandleChange}
+//                         placeholder="User Name"
+//                     />
+//                     <br />
+//                     <input
+//                         type="password"
+//                         name="password"
+//                         value={this.state.loginCred.password}
+//                         onChange={this.loginHandleChange}
+//                         placeholder="Password"
+//                     />
+//                     <br />
+//                     <button>Login</button>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+
+// const mapStateToProps = state => {
+//     return {
+//         users: state.user.users,
+//         loggedInUser: state.user.loggedInUser,
+//         isLoading: state.system.isLoading
+//     };
+// };
+// const mapDispatchToProps = {
+//     login,
+//     logout,
+//     signup,
+//     removeUser,
+//     loadUsers
+// };
+
+
+
+// export const Login = connect(mapStateToProps, mapDispatchToProps)(_Login);
