1d2f62e3ab51431f28cd2a7652b88a428a2f47e1
AlexGb <User@Users-MacBook-Pro.local>
2020-09-19 17:56:46 +0300

    s

--- frontend/src/cmps/chat.jsx
+++ frontend/src/cmps/chat.jsx
@@ -1,19 +1,34 @@
 
 import React, { Component } from 'react'
+import socketService from '../services/socketService'
+import { connect } from 'react-redux';
+import { loadPets } from '../store/actions/petActions.js';
 
-export class Chat extends Component {
+ class _Chat extends Component {
 
-    state = {
-        msg: { from: 'Me', txt: '' },
-        msgs: [],
-      };
+      async componentDidMount() {
+            await this.setState()
+            socketService.setup();
+            socketService.emit('chat topic', this.state.topic);
+            socketService.on('chat addMsg', this.addMsg);
+      }
+    
+      componentWillUnmount() {
+        socketService.off('chat addMsg', this.addMsg);
+        socketService.terminate();
+      }
+
+      sendMsg=ev=>{
+          console.log('sending');
+      }
+      msgHandleChange=ev=>{
+          console.log('change');
+      }
 
     render() {
         return (
             <div className="chat-container">
-                Chat
-            
-                {/* <form onSubmit={this.sendMsg}>
+                <form onSubmit={this.sendMsg}>
                 <input
                     type="text"
                     value={this.state.msg.txt}
@@ -26,8 +41,22 @@ export class Chat extends Component {
                 {this.state.msgs.map((msg, idx) => (
                     <li key={idx}>{msg}</li>))}
                 </ul>
-             */}
+            
             </div>
         )
     }
 }
+const mapStateToProps = state => {
+    return {
+        pets: state.petReducer.pets,
+        loggedInUser: state.userReducer.loggedInUser,
+    }
+}
+
+const mapDispatchToProps = {
+    loadPets,
+    
+
+}
+
+export const Chat = connect(mapStateToProps, mapDispatchToProps)(_Chat)
