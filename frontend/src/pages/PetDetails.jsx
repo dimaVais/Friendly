f33dd6182081f360ed460d842697155f1fb8a635
AlexGb <User@Users-MacBook-Pro.local>
2020-09-19 17:12:16 +0300

    chat

--- frontend/src/pages/PetDetails.jsx
+++ frontend/src/pages/PetDetails.jsx
@@ -89,7 +89,7 @@ class _PetDetails extends Component {
                     </div>
                     <hr />
                     <div className="shop_details">
-                    <p><span>{pet.shop.fullName}</span><button onClick={this.onChat}>Chat</button></p>
+                    {pet.shop && <p><span>{pet.shop.fullName}</span><button onClick={this.onChat}>Chat</button></p>}
                     </div>
                     <p><span>Age: </span>
                         {`${parseFloat((Date.now() - new Date(pet.bDate)) / (1000 * 60 * 60 * 24 * 30 * 12)).toFixed(1)}`}
