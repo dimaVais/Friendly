d8d2c39b3850a5b5723ae46f3975edbf4ece30c8
Asaf Dulman <asafdulman@gmail.com>
2020-09-19 23:33:00 +0300

    hey

--- frontend/src/cmps/PetPreview.jsx
+++ frontend/src/cmps/PetPreview.jsx
@@ -1,52 +1,57 @@
-import React from 'react'
 import { NavLink, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMars } from '@fortawesome/free-solid-svg-icons'
 import { faVenus } from '@fortawesome/free-solid-svg-icons'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 
-export function PetPreview(props) {
-    const { shop } = props.pet;
-    const pet = props.pet;
+class _PetPreview extends Component {
 
 
-    function displayEditButtons() {
-        // if (!props.loggedinUser.isAdmin) return <div></div>
+    state = {
+        pet: this.props.pet
+    }
+
+    // displayEditButtons = () => {
+    //     // if (!props.loggedinUser.isAdmin) return <div></div>
+
+    //     return (
+    //         <div className="btn-panel">
+    //             {/* <button className="btn btn-delete" onClick={() => { this.props.onRemove(pet._id) }}>Delete</button> */}
+    //             {/* <Link to={`edit/${pet._id}`} className="btn btn-edit"> */}
+    //                 Edit
+    //                 </Link>
+    //         </div>
+    //     )
+    // }
+
+    isMale = () => {
+        return (this.state.pet.gender === 'Male') ? true : false
+    }
+
+    render() {
+        const { pet } = this.state
 
         return (
-            <div className="btn-panel">
-                <button className="btn btn-delete" onClick={() => { props.onRemove(pet._id) }}>Delete</button>
-                <Link to={`edit/${pet._id}`} className="btn btn-edit">
-                    Edit
-                    </Link>
-            </div>
-        )
-    }
-
-    function isMale() {
-        return (pet.gender === 'Male') ? true : false
-    }
-
-    // const image = pet.img ? require(`../assets/imgs/${car._id}.jpg`) : require(`../assets/imgs/default.jpg`)
-    return (
-        <NavLink to={`/details/${pet._id}`}>
-            <div className="pet-preview">
-                <img src={(pet.imgUrls) ? pet.imgUrls[0] : ''} alt="pet" />
-                <div className="card-description">
-                    <div className="card-description-header">
-                        <h2 className="pet-name">{pet.name}</h2>
-                        {/* <img className="gender-icon" src={require(`../assets/img/male-icon2.png`)} alt="male-icon"/> */}
-                            {isMale() && <FontAwesomeIcon className="pet-gender-male-icon" icon={faMars} />} 
-                            {!isMale() && <FontAwesomeIcon className="pet-gender-female-icon" icon={faVenus} />} 
+            <NavLink to={`/details/${pet._id}`}>
+                <div className="pet-preview">
+                    <img src={(pet.imgUrls) ? pet.imgUrls[0] : ''} alt="pet" />
+                    <div className="card-description">
+                        <div className="card-description-header">
+                            <h2 className="pet-name">{pet.name}</h2>
+                            {this.isMale() && <FontAwesomeIcon className="pet-gender-male-icon" icon={faMars} />}
+                            {!this.isMale() && <FontAwesomeIcon className="pet-gender-female-icon" icon={faVenus} />}
+                        </div>
+                        <h4>{pet.summary}</h4>
                     </div>
-                    <h4>{pet.summary}</h4>
-                </div>
+                    <div className="shop-container">
+                        {pet.shop && <h4>{pet.shop.fullName}</h4>}
+                        {pet.shop && <p className="shop-rate">{pet.shop.rate}  </p>}
 
-                <section className="pet-category">
-                    {/* <img src={`../assets/img/${pet.gender.toLowerCase()}`} alt="gender"/>
-                <img src={`../assets/img/${pet.size.toLowerCase()}`} alt="size"/> */}
-                </section>
-                {/* <section className="pet-reacts">
+                    </div>
+
+                    {/* <section className="pet-reacts">
                     <ul> 
                     { pet.reactions && pet.reactions.map(reaction ,index => {
                         <li key={index}>
@@ -56,10 +61,25 @@ export function PetPreview(props) {
                     })}
                     </ul>
                 </section> */}
-                {/* {displayEditButtons()} */}
+                    {/* {displayEditButtons()} */}
 
-            </div>
-        </ NavLink>
-
-    )
+                </div>
+            </ NavLink>
+        )
+    }
 }
+
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = {
+
+
+}
+
+
+export const PetPreview = connect(mapStateToProps, mapDispatchToProps)(_PetPreview)
+
