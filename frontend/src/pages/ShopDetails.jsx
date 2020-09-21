d64de873d8986de6ee621e9285afe7f243184632
dimaVais <dimavais300@gmail.com>
2020-09-21 12:36:05 +0300

    Start Add review

--- frontend/src/pages/ShopDetails.jsx
+++ frontend/src/pages/ShopDetails.jsx
@@ -3,18 +3,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { PetList } from '../cmps/PetList';
 import { OrderList } from '../cmps/OrderList';
-import { getShopById } from '../store/actions/shopActions.js';
+import { getShopById, saveShop } from '../store/actions/shopActions.js';
 import { savePet, loadPets } from '../store/actions/petActions.js'
 import { GoogleMap } from '../cmps/GoogleMap';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
 
 class _ShopDetails extends Component {
 
     state = {
         shop: {},
-        shopPets: []
+        shopPets: [],
+        isOpenReviews: false,
+        currReview: {
+            txt: '',
+            rate: ''
+        }
     }
 
-    async componentDidMount() {
+    componentDidMount() {
+        this.loadShopData();
+    }
+
+    loadShopData = async () => {
         const shopId = this.props.match.params.id;
         await this.props.loadPets();
         await this.props.getShopById(shopId);
@@ -36,6 +49,43 @@ class _ShopDetails extends Component {
         console.log('edit');
     }
 
+    handleInput = ({ target }) => {
+        const field = target.name;
+        const value = target.value;
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                currReview: {
+                    ...prevState.currReview,
+                    [field]: value
+                }
+            }
+        })
+    }
+
+    onAddReview = async () => {
+        const review = { ...this.state.currReview };
+        const user = this.props.loggedInUser.fullName;
+        review.by = {
+            _id: user._id,
+            fullName: user.fullName,
+            imgUrl: user.imgUrl
+        }
+        const shop = { ...this.state.shop };
+        shop.reviews.push(review);
+        await this.props.saveShop(shop);
+        this.loadShopData();
+    }
+
+    onToggleReviewModal = () => {
+        this.setState(
+            {
+                ...this.state,
+                isOpenReviews: !this.state.isOpenReviews
+            }
+        )
+    }
+
     render() {
         const shop = this.state.shop;
         return (
@@ -54,35 +104,66 @@ class _ShopDetails extends Component {
 
                             </div>
                             <div className="shop-location-box">
-                            <h2><span>We are located in </span>{shop.location.name}</h2>
-                            <div className="map-container">
-                                <GoogleMap lat={shop.location.lat} lng={shop.location.lng} name={shop.location.name} />
+                                <h2><span>We are located in </span>{shop.location.name}</h2>
+                                <div className="map-container">
+                                    <GoogleMap lat={shop.location.lat} lng={shop.location.lng} name={shop.location.name} />
+                                </div>
                             </div>
-                        </div>
                             <div className="order-list-box">
                                 <OrderList isShop={true} orderFilterName={"shop._id"} filterById={this.state.shop._id} />
                             </div>
-
-
                         </div>
                         <div className="flex">
-                       
-                        <div className="reviews-box">
-                            <h3>What they say about us</h3>
-                            {(!shop.reviews) ? <h1>LOADING Reviews....</h1> :
-                                <ul>
-                                    {shop.reviews.map(review => {
-                                        return <li>
-                                            <p><span>From: </span>{review.by.fullName}</p>
-                                            <p><span>Rate: </span>{review.rate}</p>
-                                            <p>{review.txt}</p>
-                                        </li>
-                                    }
+                            <div className="reviews-box">
+                                <h3>What they say about us</h3>
+                                <button onClick={this.onToggleReviewModal}>
+                                    {(!this.state.isOpenReviews) ? 'Tell Us What You Think' : `Close review form`}
+                                </button>
+                                <div className={(this.state.isOpenReviews) ? `review-modal` : `hidden`}>
+                                    <form className="review-form">
+                                        <div className="flex column flex-end">
+                                            <TextField
+                                                id="standard-textarea"
+                                                label="Shelter/Owner Review:"
+                                                placeholder="write your review:"
+                                                multiline
+                                                name="txt"
+                                                onChange={this.handleInput}
+                                            />
+                                            <FormControl>
+                                                <InputLabel htmlFor="rate">Please add your rate:</InputLabel>
+                                                <Select
+                                                    native
+                                                    onChange={this.handleInput}
+                                                    inputProps={{
+                                                        name: "rate",
+                                                    }}>
+                                                    <option aria-label="None" value="" />
+                                                    <option value="5">5</option>
+                                                    <option value="4">4</option>
+                                                    <option value="3">3</option>
+                                                    <option value="2">2</option>
+                                                    <option value="1">1</option>
+                                                </Select>
+                                            </FormControl>
+                                        </div>
+                                        <button>Publish</button>
+                                    </form>
+                                </div>
+                                {(!shop.reviews) ? <h1>LOADING Reviews....</h1> :
+                                    <ul>
+                                        {shop.reviews.map(review => {
+                                            return <li>
+                                                <p><span>From: </span>{review.by.fullName}</p>
+                                                <p><span>Rate: </span>{review.rate}</p>
+                                                <p>{review.txt}</p>
+                                            </li>
+                                        }
 
-                                    )}
-                                </ul>}
-                        </div>
-                        
+                                        )}
+                                    </ul>}
+                            </div>
+
                         </div>
                         <div className="pets-box" >
                             <h2>Our Pets</h2>
@@ -106,6 +187,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = {
     getShopById,
+    saveShop,
     loadPets,
     savePet
 }
