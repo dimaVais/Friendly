d08872a0cee5e59fff73d5f170ed87742046dbf3
Asaf Dulman <asafdulman@gmail.com>
2020-09-23 14:17:56 +0300

    hey

--- frontend/src/cmps/PetPreview.jsx
+++ frontend/src/cmps/PetPreview.jsx
@@ -54,8 +54,8 @@ class _PetPreview extends Component {
                     <div className="shop-container">
                         {pet.shop && <Link to={`/shop/${pet.shop._id}`}>{pet.shop.fullName}</Link>}
                         {/* {pet.shop && <h4>{pet.shop.fullName}</h4>} */}
-                        <div className="pet-rate-box">
-                            <span>{pet.reacts && pet.reacts[0].type === 'love' ? <FontAwesomeIcon onClick={(ev) => { this.onUpdateReaction(ev, 'love') }} className="love-icon" icon={faHeart} /> : ''} </span>
+                        <div onClick={(ev) => { this.onUpdateReaction(ev, 'love') }} className="pet-rate-box">
+                            <span>{pet.reacts && pet.reacts[0].type === 'love' ? <FontAwesomeIcon  className="love-icon" icon={faHeart} /> : ''} </span>
                             {pet.reacts &&pet.reacts[0].type === 'love' ? <span className="love-count">{pet.reacts[0].count}</span> : ''}
 
 
