c014b75e41373d4260802fefda55064f11a23747
dimaVais <dimavais300@gmail.com>
2020-09-30 10:34:06 +0300

    Add connect

--- frontend/src/cmps/OrderPreview.jsx
+++ frontend/src/cmps/OrderPreview.jsx
@@ -1,5 +1,5 @@
-
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfoCircle, faComment, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
