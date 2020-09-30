aeeec44bce0e8e0f87d5c7854538c80dd9597f26
Asaf Dulman <asafdulman@gmail.com>
2020-09-30 11:54:03 +0300

    he

--- frontend/src/cmps/OrderPreview.jsx
+++ frontend/src/cmps/OrderPreview.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInfoCircle, faComment, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import userService from '../services/userService';
